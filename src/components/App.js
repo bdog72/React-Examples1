029efecc3587a9dd8c10f82b0f62192d389316c7
Brian Bycynski <high5bri@hotmail.com>
2017-06-24 05:47:15 -0400

    made a few changes to component example

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 
+const CREDITCARD = 'CreditCard'
+const BTC = 'Bitcoin'
+
 class App extends Component {
+  state = {
+    payMethod: BTC
+  }
+
+  select = (choice) => {
+    return (evt) => {
+      this.setState({
+        payMethod: choice
+      })
+    }
+  }
+
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <div className='switch'>
+        <div className='choice'
+          onClick={this.select(CREDITCARD)}>
+          Creditcard
+        </div>
+        <div className='choice'
+          onClick={this.select(BTC)}>
+          Bitcoin
+        </div>
+        Pay with: {this.state.payMethod}
+      </div>
     </div>
   }
 }
