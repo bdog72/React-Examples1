545ce761884cb698a4ffea030f94aab689343dab
Brian Bycynski <high5bri@hotmail.com>
2017-06-25 07:21:15 -0400

    made first button example

--- src/components/BasicButton1.js
+++ src/components/BasicButton1.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 
 export default class BasicButton1 extends Component {
+  onGreatClick = (evt) => {
+    console.log('The user clicked button-1: great', evt)
+  }
+
+  onAmazingClick = (evt) => {
+    console.log('The user clicked button-2: amazing', evt)
+  }
+
   render () {
     return <div>
-      What up dog
+      <h1>What Do You Think Of React?</h1>
+      <h2>Click a button and check the console</h2>
+      <button
+        name='button-1'
+        value='great'
+        onClick={this.onGreatClick}>
+        Great
+      </button>
+      <button
+        name='button-2'
+        value='amazing'
+        onClick={this.onAmazingClick}>
+          Amazing
+      </button>
     </div>
   }
 }
