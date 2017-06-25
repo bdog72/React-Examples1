5add06810398fc59da1729261f7ae33fbd173b5d
Brian Bycynski <high5bri@hotmail.com>
2017-06-25 08:53:01 -0400

    made second button example

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Components from './Components'
 import BasicButton1 from './BasicButton1'
+import BasicButton2 from './BasicButton2'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 export default class App extends Component {
@@ -11,11 +12,12 @@ export default class App extends Component {
         <div>
           <ul>
             <li><Link to='/'>React Component</Link></li>
-            <li><Link to='/ex2'>Basic Button</Link></li>
-
+            <li><Link to='/ex2'>Basic Button 1</Link></li>
+            <li><Link to='/ex3'>Basic Button 2</Link></li>
             <hr />
             <Route exact path='/' component={Ex1} />
             <Route path='/ex2' component={Ex2} />
+            <Route path='/ex3' component={Ex3} />
           </ul>
         </div>
       </Router>
@@ -28,17 +30,24 @@ const Ex1 = () => (
     <Components />
   </div>
 )
+
 const Ex2 = () => (
   <div>
     <BasicButton1 />
   </div>
 )
+
+const Ex3 = () => (
+  <div>
+    <BasicButton2 />
+  </div>
+)
+
 const styles1 = {
   app: {
     paddingTop: 20,
     textAlign: 'center',
     fontSize: 50
-    // marginbottom: 20
   }
 }
 
---
+++ src/components/BasicButton2.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class BasicButton2 extends Component {
+  onButtonClick = (evt) => {
+    const btn = evt.target
+    console.log(`The user clicked ${btn.name} ${btn.value}`)
+  }
+  render () {
+    return <div>
+      <h1>What do you think of react</h1>
+      <h2>Click a button and check the console</h2>
+      <button
+        name='button-1'
+        value='Awesome'
+        onClick={this.onButtonClick}>
+        Awesome
+      </button>
+      <button
+        name='button-2'
+        value='Fantastic'
+        onClick={this.onButtonClick}>
+        Fantastic
+      </button>
+    </div>
+  }
+}
